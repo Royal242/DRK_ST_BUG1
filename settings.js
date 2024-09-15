ae49944536db25f04dd3fecb4d9f624ca8f8ddd8
Royal242 <jeancymoyele0@gmail.com>
2024-09-15 20:57:15 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,14 +2,14 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "50931461936"
-global.ownername = "🌹⃟⃢👑 ĐÆ𝚪𝐊༒𝙎-𝙏𝞢𝞜🌹⃟⃢👑"
+global.ownernomer = "242044190987"
+global.ownername = "𝐵𝐿𝛩𝛩𝐷 𝐽𝛩𝛫𝛯𝑅"
 global.ytname = "DRK_ST_Tech"
 global.socialm = "GitHub: DRK-S-TEN"
 global.location = "America, Haiti, Port-au-Prince"
 
-global.ownernumber = '50931461936'  //creator number
-global.ownername = '𝙎-𝙏𝞢𝞜' //owner name
+global.ownernumber = '242044190987'  //creator number
+global.ownername = '𝐵𝐿𝛩𝛩𝐷 𝐽𝛩𝛫𝛯𝑅' //owner name
 global.botname = 'DRK_ST_BUG' //name of the bot
 
 //sticker details
@@ -29,11 +29,11 @@ global.prefa = ['']
 //false=disable and true=enable
 global.autoRecording = false //auto recording
 global.autoTyping = false //auto typing
-global.autorecordtype = true //auto typing + recording
+global.autorecordtype = false //auto typing + recording
 global.autoread = false //auto read messages
 global.autobio = false //auto update bio
 global.anti92 = false //auto block +92 
-global.autoswview = true //auto view status/story
+global.autoswview = false //auto view status/story
 
 //menu type 
 //v1 is image menu, 
